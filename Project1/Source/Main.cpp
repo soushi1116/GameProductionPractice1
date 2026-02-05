7531cd2b4484435ccfebff08c08950a96a6473cd
soushi1116 <kurosawasoushi@gmail.com>
2026-02-05 10:14:42 +0900

    GameSetting.h作成

---
+++ Project1/Source/Main.cpp
@@ -0,0 +1,7 @@
+#include "Dxlib.h"
+#include "Scene/SceneManager.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR IpCmdLine, _In_ int nShowCmd)
+{
+	ChangeWindowMode(TRUE);
+}
