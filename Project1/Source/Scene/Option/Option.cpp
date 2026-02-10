00712205ae0ec9cdb32d2e37e991f26c0c634fa3
Raytraitor <y20061208@gmail.com>
2026-02-10 14:43:05 +0900

    a

---
+++ Project1/Source/Scene/Option/Option.cpp
@@ -0,0 +1,57 @@
+#include "DxLib.h"
+#include "Option.h"
+#include "../SceneManager.h"
+
+
+// 画像ハンドル
+int bg;
+int option;
+int bgm;
+int se;
+int gauge;
+
+void OptionInit()
+{
+    // 画像を読み込む
+    bg = LoadGraph("Data/Option/BG.png");
+    option = LoadGraph("Data/Option/Option.png");
+    bgm = LoadGraph("Data/Option/BGM.png");
+    se = LoadGraph("Data/Option/SE.png");
+    gauge = LoadGraph("Data/Option/Gauge.png");
+}
+
+void OptionUpdate()
+{
+    // Cキーを押したらタイトルに戻る
+    if (CheckHitKey(KEY_INPUT_C))
+    {
+        ChangeScene(SCENE_TITLE);
+    }
+}
+
+void OptionDraw()
+{
+    // 背景
+    DrawGraph(0, 0, bg, TRUE);
+
+    // Option
+    DrawGraph(500, 50, option, TRUE);
+
+    // BGM
+    DrawGraph(400, 250, bgm, TRUE);
+    DrawGraph(700, 250, gauge, TRUE);
+
+    // SE
+    DrawGraph(400, 350, se, TRUE);
+    DrawGraph(700, 350, gauge, TRUE);
+}
+
+void OptionFin()
+{
+    // 画像を消す
+    DeleteGraph(bg);
+    DeleteGraph(option);
+    DeleteGraph(bgm);
+    DeleteGraph(se);
+    DeleteGraph(gauge);
+}
